26a239fb952f410836534cc3deaf8c752153a7d4
kongr45gpen <kongr45gpen@helit.org>
2013-01-02 14:56:53 +0200

    Added version for 2.0.x

--- flagOnSpawn.cpp
+++ flagOnSpawn.cpp
@@ -1,58 +1,57 @@
-// bzspawnflagvar.cpp : Defines the entry point for the DLL application.
+// flagOnSpawn.cpp : Defines the entry point for the DLL application.
 //
 
 #include "bzfsAPI.h"
 #include "plugin_utils.h"
-#include <stdlib.h>
 
-class bzspawnflagvar : public bz_Plugin
+BZ_GET_PLUGIN_VERSION
+
+class flagOnSpawn_events : public bz_EventHandler
 {
-  virtual const char* Name (){return "Flag on Spawn";}
-  virtual void Init ( const char* config);
-
-  virtual void Event ( bz_EventData * eventData );
-};
-
-BZ_PLUGIN(bzspawnflagvar)
-
-void bzspawnflagvar::Init ( const char* /*commandLine*/ )
-{
-  bz_debugMessage(4,"flagOnSpawn plugin loaded");
-  if (!bz_BZDBItemExists("_flagonspawn") && bz_setBZDBString ( "_flagonspawn", "none", 0 , 0 ))
-  {  
-	bz_debugMessage(4,"flagOnSpawn plugin set");
-  } else  {
-	bz_debugMessage(4,"flagOnSpawn plugin failed or already set");
-  }
-  Register(bz_ePlayerSpawnEvent);
-}
-
-void bzspawnflagvar::Event ( bz_EventData *eventData )
-{
-    if(eventData->eventType==bz_ePlayerSpawnEvent) {
+  virtual void process ( bz_EventData *eventData )
+  {
+    if(eventData->eventType==bz_ePlayerSpawnEvent)
+    {
       //Here we read the _flagonspawn variable
       std::string flaggive=(bz_getBZDBString("_flagonspawn")).c_str();
-      const int maxrounds = 20;
-      if(flaggive == "random") {
-        for(int i=0; i<maxrounds; i++) {
-          int flagid = rand() % bz_getNumFlags();
-          if(bz_flagPlayer(flagid) != -1 && i != maxrounds-1) continue;
-          
-          //There is an unused flag. Give a similar one to the player.
-          bz_PlayerSpawnEventData_V1* data = (bz_PlayerSpawnEventData_V1*)eventData;
-          bz_givePlayerFlag(data->playerID,bz_getName(flagid).c_str(),1);
-          bz_debugMessage(4,"gave random flag");
-          break;
-        }
-
-      } else if(flaggive != "none") {
-         //Here we give a flag to the player
-         bz_PlayerSpawnEventData_V1* data = (bz_PlayerSpawnEventData_V1*)eventData;
-         bz_givePlayerFlag(data->playerID,flaggive.c_str(),1);
-         bz_debugMessage(4,"gave flag");
+      if(flaggive!="AA")
+      {
+﻿   //Here we give a flag to the player
+﻿   bz_PlayerSpawnEventData* data = (bz_PlayerSpawnEventData*)eventData;
+﻿   bz_givePlayerFlag(data->playerID,flaggive.c_str(),1);
+﻿   bz_debugMessage(4,"gave flag");
       }
     }
+  }
+};
+
+flagOnSpawn_events flagOnSpawn_events;
+BZF_PLUGIN_CALL int bz_Load ( const char* /*commandLine*/ )
+{
+  bz_debugMessage(4,"flagOnSpawn plugin loaded");
+      if (bz_setBZDBString ( "_flagonspawn", "AA", 0 , 0 ))
+ ﻿  {  
+﻿    bz_debugMessage(4,"flagOnSpawn plugin set");
+﻿  }
+      else
+﻿  {
+﻿    bz_debugMessage(3,"flagOnSpawn plugin failed");
+﻿  }
+
+  bz_registerEvent(bz_ePlayerSpawnEvent,&flagOnSpawn_events);
+  return 0;
+
+
 }
+
+BZF_PLUGIN_CALL int bz_Unload ( void )
+{
+  bz_debugMessage(4,"flagOnSpawn plugin unloaded");
+  bz_removeEvent(bz_ePlayerSpawnEvent,&flagOnSpawn_events);
+  return 0;
+}
+
+//plugin by alezakos
 
 // Local Variables: ***
 // mode:C++ ***
@@ -61,3 +60,5 @@ void bzspawnflagvar::Event ( bz_EventData *eventData )
 // indent-tabs-mode: t ***
 // End: ***
 // ex: shiftwidth=2 tabstop=8
+
+
