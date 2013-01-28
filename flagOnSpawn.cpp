d57d43a58248e41fbc5a16604534c51b4c88fd6c
kongr45gpen <kongr45gpen@helit.org>
2013-01-28 14:57:56 +0200

    Fixed ambiguous function name

--- flagOnSpawn.cpp
+++ flagOnSpawn.cpp
@@ -19,7 +19,7 @@ void bzspawnflagvar::Init ( const char* /*commandLine*/ )
 {
   bz_debugMessage(4,"flagOnSpawn plugin loaded");
   if (!bz_BZDBItemExists("_flagonspawn") && bz_setBZDBString ( "_flagonspawn", "none", 0 , 0 ))
-  {  
+  {
 	bz_debugMessage(4,"flagOnSpawn plugin set");
   } else  {
 	bz_debugMessage(4,"flagOnSpawn plugin failed or already set");
@@ -37,10 +37,10 @@ void bzspawnflagvar::Event ( bz_EventData *eventData )
         for(int i=0; i<maxrounds; i++) {
           int flagid = rand() % bz_getNumFlags();
           if(bz_flagPlayer(flagid) != -1 && i != maxrounds-1) continue;
-          
+
           //There is an unused flag. Give a similar one to the player.
           bz_PlayerSpawnEventData_V1* data = (bz_PlayerSpawnEventData_V1*)eventData;
-          bz_givePlayerFlag(data->playerID,bz_getName(flagid).c_str(),1);
+          bz_givePlayerFlag(data->playerID,bz_getFlagName(flagid).c_str(),1);
           bz_debugMessage(4,"gave random flag");
           break;
         }
